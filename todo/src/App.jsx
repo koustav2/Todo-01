84d86a761e3b291327f0ede795acae6044366288
koustav <koustavm.aeie2018@nsec.ac.in>
2023-05-27 18:50:06 +0530

    style

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
-
+import { useSelector } from "react-redux";
 import './App.css'
 import Body from './components/Body'
+import Category from './components/categoty/Category';
 
 function App() {
-
+  const todosItem = useSelector((state) => state.todos.todosList);
   return (
-    <div className='w-full min-h-screen pt-4 flex glex-col gap-10 justify-center items-center font-bodyFont bg-gradient-to-t from-sky-100 via-sky-400 to-sky-500 text-white px-4'>
-      <div  className='w-[850px] h-full bg-bodyColor p-10 '>
-        <Body/>
+    <div className='w-full min-h-screen pt-4 flex flex-col gap-10 justify-center items-center font-bodyFont bg-gradient-to-t from-sky-100 via-sky-400 to-sky-500 text-white px-4'>
+      {
+        todosItem.length > 0 ? <Category /> : ""
+      }
+      <div className='w-[850px] h-full bg-bodyColor p-10 '>
+        <Body />
       </div>
     </div>
   )
